a242bee0fa86df361bbf5c67008d7e9e1e6cbc94
Aaron Whaley <aaron@Aarons-MacBook-Pro.local>
2017-05-03 01:55:14 -0500

    Can add oil changes to iPhone's built-in calendar app now

---
+++ AutoMaintenanceTracker/AutoMaintenanceTracker/EventAddedDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  EventAddedDelegate.swift
+//  AutoMaintenanceTracker
+//
+//  Created by Aaron Whaley on 5/3/17.
+//  Copyright © 2017 Mohammed Diab. All rights reserved.
+//
+
+import Foundation
+protocol EventAddedDelegate {
+    func eventDidAdd()
+}
